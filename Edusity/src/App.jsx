da2738d7c36cee53abf3923e35e5ad17a6f9dbbf
Pradeep Pandit <pp8379951@gmail.com>
2026-06-06 22:55:03 +0530

    Change

--- Edusity/src/App.jsx
+++ Edusity/src/App.jsx
@@ -4,18 +4,25 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
         <Title subTitle="Our Program" title="What We Offer" />
-      <Programs/>
-      <About/>
+        <Programs />
+        <About />
+
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus/>
+
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
+
       </div>
-      
+
     </div>
   )
 }
